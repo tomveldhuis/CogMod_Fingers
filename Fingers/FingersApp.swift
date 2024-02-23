7d4139212bc75dd6e62ff6f2c64d9de4e995ba62
Tom Veldhuis <tomveldhuis@live.nl>
2024-02-23 11:16:13 +0100

    Initial Commit

---
+++ Fingers/FingersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FingersApp.swift
+//  Fingers
+//
+//  Created by Tom on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FingersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
