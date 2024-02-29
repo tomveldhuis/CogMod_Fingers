4a5579cb1870e4c61b5e28858970b5804da9b308
tomveldhuis <49715145+tomveldhuis@users.noreply.github.com>
2024-02-29 17:46:56 +0100

    added a circle around the players

--- Fingers/FingersView.swift
+++ Fingers/FingersView.swift
@@ -10,38 +10,51 @@ import SwiftUI
 struct FingersView: View {
     @ObservedObject var fingersGame: FingersViewModel
     
+    // Represents a player in the view
     private struct Player: Identifiable {
         let name: String
         let position: CGPoint?
         var id: String { name }
     }
     
-    private func findPlayers(n: Int) -> [Player] {
-        let x = UIScreen.main.bounds.width / 2
-        let y = UIScreen.main.bounds.height / 2
+    // Find the coordinates for players in the view
+    private func findPlayers(n: Int, bounds: CGSize, circleSize: CGFloat) -> [Player] {
+        let x = bounds.width / 2
+        let y = bounds.height / 2
         var startAngle = Angle.degrees(-90)
         let angleInc = Angle.degrees(Double(360 / n))
         var players: [Player] = []
 
         var path = Path()
         for id in 1...n {
-            path.addArc(center: CGPoint(x: x, y: y), radius: x - 50, startAngle: startAngle, endAngle: startAngle + angleInc, clockwise: true)
+            path.addArc(center: CGPoint(x: x, y: y), radius: x - circleSize, startAngle: startAngle, endAngle: startAngle + angleInc, clockwise: true)
             players.append(Player(name: id.description, position: path.currentPoint))
             startAngle += angleInc
         }
         return players
     }
     
+    // The ContentView
     var body: some View {
-        let players = findPlayers(n: 5)
-        
-        return ZStack(content: {
-            ForEach(players) { player in
+        GeometryReader { proxy in
+            // Parameters
+            let circleSize = 50
+            let nr_players = 8
+            
+            let size = proxy.size
+            let players = findPlayers(n: nr_players, bounds: size, circleSize: CGFloat(circleSize))
+            
+            ZStack(content: {
                 Circle()
-                    .frame(width: 50, height: 50)
-                    .position(player.position!)
-            }
-        })
+                    .stroke(.red, lineWidth: 5)
+                    .frame(width: size.width - 2 * CGFloat(circleSize), height: size.height - 2 * CGFloat(circleSize))
+                ForEach(players) { player in
+                    Circle()
+                        .frame(width: CGFloat(circleSize), height: CGFloat(circleSize))
+                        .position(player.position!)
+                }
+            })
+        }
     }
 }
 
