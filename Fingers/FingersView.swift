57c2359c941a62efb61315071cd169437717d471
tomveldhuis <49715145+tomveldhuis@users.noreply.github.com>
2024-02-29 16:56:20 +0100

    added scalable players in View

--- Fingers/FingersView.swift
+++ Fingers/FingersView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  FingersView.swift
 //  Fingers
 //
 //  Created by Tom on 19/02/2024.
@@ -10,9 +10,37 @@ import SwiftUI
 struct FingersView: View {
     @ObservedObject var fingersGame: FingersViewModel
     
+    private struct Player: Identifiable {
+        let name: String
+        let position: CGPoint?
+        var id: String { name }
+    }
+    
+    private func findPlayers(n: Int) -> [Player] {
+        let x = UIScreen.main.bounds.width / 2
+        let y = UIScreen.main.bounds.height / 2
+        var startAngle = Angle.degrees(-90)
+        let angleInc = Angle.degrees(Double(360 / n))
+        var players: [Player] = []
+
+        var path = Path()
+        for id in 1...n {
+            path.addArc(center: CGPoint(x: x, y: y), radius: x - 50, startAngle: startAngle, endAngle: startAngle + angleInc, clockwise: true)
+            players.append(Player(name: id.description, position: path.currentPoint))
+            startAngle += angleInc
+        }
+        return players
+    }
+    
     var body: some View {
+        let players = findPlayers(n: 5)
+        
         return ZStack(content: {
-            
+            ForEach(players) { player in
+                Circle()
+                    .frame(width: 50, height: 50)
+                    .position(player.position!)
+            }
         })
     }
 }
