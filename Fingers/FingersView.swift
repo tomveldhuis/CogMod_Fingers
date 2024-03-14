f6aef34d91487bdae4aeaa758674153374a6d72e
tomveldhuis <49715145+tomveldhuis@users.noreply.github.com>
2024-03-14 09:06:43 +0100

    added timer to View

--- Fingers/FingersView.swift
+++ Fingers/FingersView.swift
@@ -7,31 +7,136 @@
 
 import SwiftUI
 
+enum gameState {
+    case Initial
+    case Predict
+    case Countdown
+    case Result
+}
+
 struct PlayerView: Identifiable {
     let name: String
     let position: CGPoint?
     var id: String { name }
+    @State var pressed: Bool = false
     
     func getButton(size: CGFloat) -> some View {
-        return Circle()
-            .frame(width: size, height: size)
-            .position(self.position!)
-            .onLongPressGesture(minimumDuration: .infinity) {
-                //print("Finished")
-            } onPressingChanged: { isPressing in
-                if isPressing {
-                    print(self.id)
-                } else {
-                    print("Finished")
-                }
+        return ZStack {
+            Circle()
+            Text(id.description)
+                .foregroundColor(.white)
+        }
+        .frame(width: size, height: size)
+        .position(self.position!)
+        .onLongPressGesture(minimumDuration: .infinity) {
+            //print("Finished")
+        } onPressingChanged: { isPressing in
+            if isPressing {
+                pressed = true
+                print(self.id)
+            } else {
+                pressed = false
+                print("Finished")
             }
+        }
     }
 }
 
 struct FingersView: View {
+    //---------------------------------
+    //----------- Properties ----------
+    //---------------------------------
+    
     @ObservedObject var fingersGame: FingersViewModel
-    @State private var showPredictPopup = false
+    @State private var state = gameState.Initial
     @State private var selectedNumberIndex: Int? = nil
+    @State private var counter = 3
+    @State private var textToUpdate = ""
+    
+    let nr_players = 5
+    let circleSize = 50
+    
+    //---------------------------------
+    //---------- Content view ---------
+    //---------------------------------
+    
+    var body: some View {
+        GeometryReader { proxy in
+            // Parameters
+            let size = proxy.size
+            let players = findPlayers(n: nr_players, bounds: size, circleSize: CGFloat(circleSize))
+
+            // View of the screen
+            VStack(content:{
+                // ----------------------------------------------
+                // -------- View of the game environment --------
+                // ----------------------------------------------
+                
+                ZStack(content: {
+                    // Big red circle
+                    Circle()
+                        .stroke(.red, lineWidth: 5)
+                        .frame(width: size.width - 2 * CGFloat(circleSize), height: size.height)
+                    
+                    // Circle for each player
+                    ForEach(players) { player in
+                        player.getButton(size: CGFloat(circleSize))
+                    }
+                    
+                    // Logic for countdown timer
+                    if state == gameState.Countdown {
+                        let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+                        Text(textToUpdate)
+                            .font(.system(size: 48))
+                            .onReceive(timer) { time in
+                                if counter == 0 {
+                                    state = gameState.Result
+                                    timer.upstream.connect().cancel()
+                                } else {
+                                    textToUpdate = counter.description
+                                    counter -= 1
+                                }
+                            }
+                    }
+                    if state == gameState.Result {
+                        Text("Time!")
+                            .font(.system(size: 48))
+                    }
+                    
+                })
+                .frame(width: size.width, height: size.height / 2)
+                
+                //---------------------------------
+                //----------- View logic ----------
+                //---------------------------------
+                
+                switch(state){
+                    case .Initial:
+                        Button("Predict") {
+                            self.state = gameState.Predict
+                        }
+                        .position(x: size.width / 2, y: size.width / 2)
+                    case .Predict:
+                        PredictView(
+                            buttons: generateNumberedButtons(numberOfPlayers: nr_players)
+                        )
+                        .padding()
+                        .cornerRadius(20)
+                        //.opacity(showPredictPopup ? 1 : 0)
+                    case .Countdown:
+                        let prediction = selectedNumberIndex!.description
+                        Text("Player predicted \(prediction) fingers remaining")
+                            .position(x: size.width / 2, y: size.width / 2)
+                    case .Result:
+                        Text("")
+                }
+            })
+        }
+    }
+
+    //---------------------------------
+    //----------- Functions -----------
+    //---------------------------------
     
     // Find the coordinates for players in the view
     private func findPlayers(n: Int, bounds: CGSize, circleSize: CGFloat) -> [PlayerView] {
@@ -50,69 +155,22 @@ struct FingersView: View {
         return players
     }
     
+    // Generate numbered buttons for PredictView
     private func generateNumberedButtons(numberOfPlayers: Int) -> [NumberButton] {
         var buttons: [NumberButton] = []
         for index in 0..<numberOfPlayers+1 {
             let button = NumberButton(label: "\(index)", action: {
                 print("Player predicted \(index) remaining")
                 selectedNumberIndex = index
-                showPredictPopup = false
+                state = gameState.Countdown
             })
             buttons.append(button)
         }
         return buttons
     }
     
-    // The ContentView
-    var body: some View {
+    private func printPressed() {
         
-        let nr_players = 8
-        
-        GeometryReader { proxy in
-            // Parameters
-            let circleSize = 50
-            
-            let size = proxy.size
-            let players = findPlayers(n: nr_players, bounds: size, circleSize: CGFloat(circleSize))
-            
-            ZStack(content: {
-                // Big red circle
-                Circle()
-                    .stroke(.red, lineWidth: 5)
-                    .frame(width: size.width - 2 * CGFloat(circleSize), height: size.height - 2 * CGFloat(circleSize))
-                
-                ForEach(players) { player in
-                    // Circle for each player
-                    player.getButton(size: CGFloat(circleSize))
-                }
-                
-                
-            })
-        }
-        
-        // Prediction overlay
-        ZStack {
-            VStack {
-                if showPredictPopup {
-                    PredictView(
-                        buttons: generateNumberedButtons(numberOfPlayers: nr_players)
-                    )
-                    .padding()
-                    .cornerRadius(20)
-                    //.opacity(showPredictPopup ? 1 : 0)
-                }
-                else if let prediction = selectedNumberIndex {
-                    Text("Player predicted \(prediction) fingers remaining")
-                }
-                else {
-                    Button("Predict") {
-                        self.showPredictPopup.toggle()
-                    }
-                }
-            }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.clear)
     }
 }
 
--- Fingers/FingersViewModel.swift
+++ Fingers/FingersViewModel.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 class FingersViewModel: ObservableObject {
     private var model: FingersModel
-    
+    var runCount: Int
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     init() {
-        model = FingersModel()
+        self.model = FingersModel()
+        self.runCount = 3
+        //_ = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+        //self.runTimer()
     }
+    
+    
+    
+//    func runTimer() {
+//        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+//            if self.runCount == 0 {
+//                print("Finished!")
+//                timer.invalidate()
+//            } else {
+//                print(self.runCount)
+//                self.runCount -= 1
+//            }
+//        }
+//    }
 }
